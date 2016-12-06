52ca35cd676b7614eac97df8650d3261b4a70845
Lance Bragstad <lbragstad@gmail.com>
2016-12-06 13:04:38 +0000

    Improve authenticate.py
    The script being used to get tokens didn't account for authenticating
    within the same second as a revocation event. Now it does.

--- authenticate.py
+++ authenticate.py
@@ -1,18 +1,35 @@
 import json
+import time
 
 import requests
+
+
+TOKEN_PATH = 'http://172.29.236.100:5000/v3/auth/tokens'
 
 
 def authenticate():
     with open('auth.json', 'r') as f:
         body = json.loads(f.read())
     headers = {'Content-Type': 'application/json'}
-    response = requests.post('http://172.29.236.100:5000/v3/auth/tokens',
-                             json=body,
-                             headers=headers)
+    response = requests.post(TOKEN_PATH, json=body, headers=headers)
     assert response.status_code == 201
-    print (response.headers.get('X-Subject-Token'))
+    return response.headers.get('X-Subject-Token')
+
+
+def validate(token):
+    headers = {
+        'X-Subject-Token': token,
+        'X-Auth-Token': token
+    }
+    return requests.get(TOKEN_PATH, headers=headers)
 
 
 if __name__ == '__main__':
-    authenticate()
+    token = authenticate()
+    response = validate(token)
+    while response.status_code == 401:
+        time.sleep(1)
+        token = authenticate()
+        response = validate(token)
+    else:
+        print token
